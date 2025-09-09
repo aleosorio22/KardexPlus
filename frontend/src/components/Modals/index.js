0bca5b4bc808e7a21bd4b51fdb5c8e3a7152b4ea
aleosorio22 <alejandroosorio022@gmail.com>
2025-09-09 16:12:15 -0600

    feat(backend & frontend): Gestion de items completa en el frontend y en el backend, lista para utilizar > Pendiente corregir algunos detalles en el frontend, pero ya funciona

--- frontend/src/components/Modals/index.js
+++ frontend/src/components/Modals/index.js
@@ -2,3 +2,4 @@ export { default as UserFormModal } from './UserFormModal';
 export { default as CategoryFormModal } from './CategoryFormModal';
 export { default as UnidadMedidaFormModal } from './UnidadMedidaFormModal';
 export { default as PresentacionFormModal } from './PresentacionFormModal';
+export { default as ItemFormModal } from './ItemFormModal';
