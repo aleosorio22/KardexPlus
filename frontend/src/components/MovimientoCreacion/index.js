15d3c6d01cfbddddf5593b1e74d7e5cdcdf18e96
aleosorio22 <alejandroosorio022@gmail.com>
2025-09-18 14:22:26 -0600

    feat(existencias): Se mejoro el frontend y el UI/UX, especialmente para mobile

--- frontend/src/components/MovimientoCreacion/index.js
+++ frontend/src/components/MovimientoCreacion/index.js
@@ -6,6 +6,9 @@
 export { default as SearchProducto } from './SearchProducto';
 export { default as ItemSelector } from './ItemSelector';
 export { default as TablaItems } from './TablaItems';
+export { default as HeaderMovimiento } from './HeaderMovimiento';
+export { default as FormularioMovimiento } from './FormularioMovimiento';
+export { default as AccionesMovimiento } from './AccionesMovimiento';
 
 // Re-exportar servicios relacionados para conveniencia
 export { existenciaService } from '../../services/existenciaService';
